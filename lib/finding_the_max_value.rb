dcfd79b0b2ff720f44c8f003781dff26b8a25657
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:14:03 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,11 @@
 def find_max_value(array)
-  count = 0
-  max = [0]
-  while count < array.length do
-    if (array[count] > max)
-      max = array[count]
+    count = 0
+    max = array[0]
+    while count < array.length do
+      if (array[count] > max)
+        max = array[count]
     end
+    count += 1
+    end
+    puts max
 end
